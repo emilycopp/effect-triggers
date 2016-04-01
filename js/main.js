05455daec7b13d2cf3ce895fce7efa1e08ecf64e
emilycopp <emily.copp@bell.net>
2016-04-01 16:47:26 -0400

    javascript

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.btn-show-hide').on('click', function () {
+  $('.box').toggleClass('show');
+});
+
+$('.btn-move').on('click', function () {
+  $('.diamond').toggleClass('move');
+});
+
+$('.btn-collapse-expand').on('click', function () {
+  $('.panel').toggleClass('collapse');
+});
+
+$('.btn-bounce').on('click', function () {
+  $('.circle').addClass('bounce');
+});
+
+$('.circle').on('animationend', function () {
+  $(this).removeClass('bounce');
+});
+
+$('.btn-append').on('click', function () {
+  $('.list').append('<li>New List Item</li>');
+});
